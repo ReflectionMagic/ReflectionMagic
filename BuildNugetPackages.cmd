b83cb388a08b9aae5f69f63dfef653310ae5512f
Jos van der Til <jos@vandertil.net>
2016-11-22 17:12:10 +0100

    Enable unit tests for net451

--- BuildNugetPackages.cmd
+++ BuildNugetPackages.cmd
@@ -1,4 +1,5 @@
 dotnet restore src/ReflectionMagic
 dotnet restore test/ReflectionMagicTests
-dotnet test test/ReflectionMagicTests
+dotnet test -f netcoreapp1.0 test/ReflectionMagicTests
+dotnet test -f net451 test/ReflectionMagicTests
 dotnet pack -c Release -o . src/ReflectionMagic
