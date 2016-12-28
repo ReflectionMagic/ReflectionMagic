35b1edd290d3be415659156d5e56735f4ba71167
Jos van der Til <jos@vandertil.net>
2016-12-28 12:29:46 +0100

    Add configuration parameter to build script.
    This will allow the build server to control the configuration through an
    environment variable. By default a Release build will be done.

--- BuildNugetPackages.cmd
+++ BuildNugetPackages.cmd
@@ -1,4 +1,7 @@
 @echo off
+IF "%CI_CONFIG%"=="" (SET CI_CONFIG=Release) 
+echo Starting build in %CI_CONFIG% mode.
+
 dotnet restore 
-dotnet test test/ReflectionMagicTests
-dotnet pack --version-suffix "%CI_VERSION%" -c Release -o . src/ReflectionMagic
+dotnet test -c %CI_CONFIG% test/ReflectionMagicTests
+dotnet pack --version-suffix "%CI_VERSION%" -c %CI_CONFIG% -o . src/ReflectionMagic
