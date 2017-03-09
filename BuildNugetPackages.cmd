fa34df6d64a6ce90763195d9b2113181141ab0aa
Jos van der Til <jos@vandertil.net>
2017-03-09 16:34:41 +0100

    Update build script for RTM dotnet CLI

--- BuildNugetPackages.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-IF "%CI_CONFIG%"=="" (SET CI_CONFIG=Release) 
-echo Starting build in %CI_CONFIG% mode.
-
-dotnet restore 
-dotnet test -c %CI_CONFIG% test/ReflectionMagicTests
-dotnet pack --version-suffix "%CI_VERSION%" -c %CI_CONFIG% -o . src/ReflectionMagic
---
+++ build-nuget.cmd
@@ -0,0 +1,7 @@
+@echo off
+IF "%CI_CONFIG%"=="" (SET CI_CONFIG=Release) 
+echo Starting build in %CI_CONFIG% mode.
+
+dotnet restore 
+dotnet test -c %CI_CONFIG% test/ReflectionMagicTests/ReflectionMagicTests.csproj
+dotnet pack --version-suffix "%CI_VERSION%" -c %CI_CONFIG% -o ..\..\ src/ReflectionMagic
