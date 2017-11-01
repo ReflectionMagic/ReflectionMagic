fc0d10345f7e8cf097fd844a0d8b538b44daa6f1
Jos van der Til <jos@vandertil.net>
2017-11-01 23:06:20 +0100

    Fix build script when version suffix is blank

--- build-nuget.cmd
+++ build-nuget.cmd
@@ -4,4 +4,4 @@ echo Starting build in %CI_CONFIG% mode.
 
 dotnet restore 
 dotnet test -c %CI_CONFIG% test/ReflectionMagicTests/ReflectionMagicTests.csproj
-dotnet pack --version-suffix "%CI_VERSION%" -c %CI_CONFIG% -o ..\..\ src/ReflectionMagic
+dotnet pack /p:VersionSuffix="%CI_VERSION%" -c %CI_CONFIG% -o ..\..\ src/ReflectionMagic
