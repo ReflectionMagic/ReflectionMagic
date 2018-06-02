9f75d4520aec29efaead610f519d9e9c1228f54e
Jos van der Til <jos@vandertil.net>
2018-06-02 23:25:56 +0200

    Fix reporting of unit test results in build

--- build-nuget.cmd
+++ build-nuget.cmd
@@ -3,5 +3,5 @@ IF "%CI_CONFIG%"=="" (SET CI_CONFIG=Release)
 echo Starting build in %CI_CONFIG% mode.
 
 dotnet restore 
-dotnet test -c %CI_CONFIG% test/ReflectionMagicTests/ReflectionMagicTests.csproj
+dotnet test --verbosity=normal -c %CI_CONFIG% test/ReflectionMagicTests/ReflectionMagicTests.csproj
 dotnet pack /p:VersionSuffix="%CI_VERSION%" -c %CI_CONFIG% -o ..\..\ --include-symbols src/ReflectionMagic
