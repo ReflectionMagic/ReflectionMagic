a9ccb28d9becd71ebcecfc8196203abbf06f1a4f
Jos van der Til <jos@vandertil.net>
2018-09-10 19:22:09 +0200

    Reduce number of build actions when packaging

--- build-nuget.cmd
+++ build-nuget.cmd
@@ -2,6 +2,8 @@
 IF "%CI_CONFIG%"=="" (SET CI_CONFIG=Release) 
 echo Starting build in %CI_CONFIG% mode.
 
+dotnet clean -c %CI_CONFIG%
 dotnet restore 
-dotnet test --verbosity=normal -c %CI_CONFIG% test/ReflectionMagicTests/ReflectionMagicTests.csproj
-dotnet pack /p:VersionSuffix="%CI_VERSION%" -c %CI_CONFIG% -o ..\..\ --include-symbols src/ReflectionMagic
+dotnet build -c %CI_CONFIG%
+dotnet test --no-build --verbosity=normal -c %CI_CONFIG% test/ReflectionMagicTests/ReflectionMagicTests.csproj
+dotnet pack --no-build /p:VersionSuffix="%CI_VERSION%" -c %CI_CONFIG% -o ..\..\ --include-symbols src/ReflectionMagic
