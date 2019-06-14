1a3cb5699c2df32a9e484710bf87ab0c9ab56215
vuplea <radu.oancea@uipath.com>
2019-06-14 13:48:50 +0300

    Expose the unwrapping helper method in DynamicHelper static class

--- src/ReflectionMagic/PrivateReflectionUsingDynamicExtensions.cs
+++ src/ReflectionMagic/PrivateReflectionUsingDynamicExtensions.cs
@@ -64,22 +64,5 @@ namespace ReflectionMagic
 
             return assembly.GetDynamicType(typeName).New(args);
         }
-
-        /// <summary>
-        /// Unwraps the specified dynamic object.
-        /// </summary>
-        /// <param name="wrappedObject">A wrapped object</param>
-        /// <returns>The unwrapped object.</returns>
-        /// <seealso cref="PrivateReflectionDynamicObjectInstance.RealObject"/>
-        /// <seealso cref="PrivateReflectionDynamicObjectStatic.RealObject"/>
-        public static object Unwrap(dynamic wrappedObject)
-        {
-            // If it's a wrapped object, unwrap it and return the real thing.
-            if (wrappedObject is PrivateReflectionDynamicObjectBase wrapper)
-                return wrapper.RealObject;
-
-            // Otherwise, return it unchanged.
-            return wrappedObject;
-        }
     }
 }
