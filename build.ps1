90ec49375add6614c20895a6f559d10b8ce86c11
Jos van der Til <jos@vandertil.net>
2026-08-23 13:07:06 +0200

    Pass ContinuousIntegrationBuildFlag

--- build.ps1
+++ build.ps1
@@ -32,7 +32,7 @@ function Exec
 exec { & dotnet clean --configuration $BuildConfiguration }
 exec { & dotnet restore }
 exec { & dotnet tool restore }
-exec { & dotnet build --configuration $BuildConfiguration /p:VersionSuffix="$VersionSuffix" }
+exec { & dotnet build --configuration $BuildConfiguration /p:VersionSuffix="$VersionSuffix" /p:ContinuousIntegrationBuild=true }
 exec { & dotnet test --results-directory $TestResultOutputDirectory --no-build --verbosity=normal --configuration $BuildConfiguration --project ./test/ReflectionMagicTests/ReflectionMagicTests.csproj }
 exec { & dotnet pack --no-build /p:VersionSuffix="$VersionSuffix" --configuration $BuildConfiguration --output $OutputDirectory --include-symbols src/ReflectionMagic }
 exec { & dotnet validate package local **\*.nupkg }
